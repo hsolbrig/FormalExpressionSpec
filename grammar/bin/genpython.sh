e0f709ee0b18cf1e13530b4d1c8bfae686f135e3
hsolbrig <solbrig.harold@mayo.edu>
2015-06-16 12:28:29 +0200

    Addressed a bug in the antlr4 python script

--- grammar/bin/genpython.sh
+++ grammar/bin/genpython.sh
@@ -1,1 +1,3 @@
-java -jar /usr/local/lib/antlr-4.5-complete.jar -Dlanguage=Python3 -o ../../implementation/ECLparser/parser -no-listener -visitor ../ECL.g4
+# Note: there is an error in the antlr code generator that puts it in the directory *above* the target -- thus the ".empty"
+#       which must exist.
+java -jar /usr/local/lib/antlr-4.5-complete.jar -Dlanguage=Python3 -o ../../implementation/ECLparser/parser/.empty -no-listener -visitor ../ECL.g4
